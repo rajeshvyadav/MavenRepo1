4bd4b57d6a19f21e2c50d42e6ffd45caffa390d2
pavani.padilam <pavani.padilam@LAPTOP-0S3BU9QM>
2019-09-19 19:23:25 +0530

    test commit

---
+++ maven.withtestng/src/test/java/com/integratemvnwithtestng/AppTest.java
@@ -0,0 +1,19 @@
+package com.integratemvnwithtestng;
+
+import org.testng.annotations.Test;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+   @Test
+   public void simplechk(){
+	   
+	   System.out.println("simple check");
+	   
+   }
+}
